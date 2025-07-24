26b8a90e58b2a9378e92e0a5fb630d09bb68599b
Henry Chochu <hisnameishenry01@gmail.com>
2025-07-24 10:53:14 +0300

    Created associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+
+user1 = User.create!(name: "Alice")
+user2 = User.create!(name: "Bob")
+
+post1 = user1.posts.create!(
+  title: "Welcome to My Blog",
+  summary: "A short intro to my thoughts.",
+  body: "This is my first blog post using Ruby on Rails and Tailwind CSS.",
+  image_url: "https://source.unsplash.com/random/800x400"
+)
+
+post2 = user2.posts.create!(
+  title: "Second Thoughts",
+  summary: "Blogging is fun!",
+  body: "Using Hotwire makes everything feel more modern.",
+  image_url: "https://source.unsplash.com/random/800x401"
+)
+
+post1.comments.create!(body: "Great post!", user: user2)
+post2.comments.create!(body: "I agree with you!", user: user1)
