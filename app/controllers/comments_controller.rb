3999b0ee5346a583c4d8ddafde0a7e7b7f2c05aa
Henry Chochu <hisnameishenry01@gmail.com>
2025-07-26 19:57:57 +0300

    Added comment submission

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,27 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.friendly.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = params[:comment][:user_id] # Manual user ID for now
+
+    if @comment.save
+      redirect_to @post, notice: "Comment added!"
+    else
+      redirect_to @post, alert: "Comment couldn't be saved."
+    end
+  end
+
+  def destroy
+    @post = Post.friendly.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to @post, notice: "Comment deleted."
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :user_id)
+  end
 end
